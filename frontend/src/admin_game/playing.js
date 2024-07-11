20ef5c273d76b0fea7c7421fec1234160205b535
aidasmiler <aida.rodriguez.perez.1211@gmail.com>
2024-07-11 12:12:35 +0200

    is voting and count of voting and voted people

--- frontend/src/admin_game/playing.js
+++ frontend/src/admin_game/playing.js
@@ -55,7 +55,6 @@ function PlayingScreen({ data, ws, connectedUsers, assessmentInstanceId }) {
 					user.voted = true;
 				}
 			});
-			console.log(filteredGradingUsers);
 			setCurrentGradingUsers(filteredGradingUsers);
 		} catch (error) {
 			console.error("Fetch error:", error);
@@ -106,7 +105,6 @@ function PlayingScreen({ data, ws, connectedUsers, assessmentInstanceId }) {
 					</h2>
 				</div>
 			</div>
-			{/* show current graded user */}
 			<div className="row mb-2">
 				<div className="col-12 text-center">
 					<h3 style={{ marginTop: "10px", marginBottom: "10px" }}>
@@ -140,81 +138,6 @@ function PlayingScreen({ data, ws, connectedUsers, assessmentInstanceId }) {
 					</table>
 				</div>
 			)}
-
-			{/* <div className="row mb-2">
-				<div className="col-12 text-center">
-					<h2 style={{ marginTop: "10px", marginBottom: "10px" }}>
-						{questionInfo.question_title}
-					</h2>
-				</div>
-			</div> */}
-
-			{/* <div className="row mb-4 align-items-center">
-				<div className="col-1 text-center">
-					<span style={{ fontSize: "2rem", color: "#333" }}>
-						{data.question_time}
-					</span>
-				</div>
-				<div className="col-9 text-center">
-					<img
-						src={questionInfo.question_image}
-						alt="Imagen de la pregunta"
-						className="img-fluid"
-						style={{ maxHeight: "300px" }}
-					/>
-				</div>
-				<div className="col-2 text-center">
-					<span style={{ fontSize: "2rem", color: "#333" }}>
-						{data.responses} / {data.players}
-					</span>
-				</div>
-			</div> */}
-
-			{/* Respuestas en dos niveles con el mismo alto */}
-			{/* <div className="row justify-content-center">
-				{questionInfo.question_answers
-					.slice(0, 2)
-					.map((answer, index) => (
-						<div key={answer.answers_id} className="col-6 mb-2">
-							<div
-								className="text-center p-3"
-								style={{
-									backgroundColor: colors[index],
-									color: "black",
-									height: "100px",
-									display: "flex",
-									alignItems: "center",
-									justifyContent: "center",
-									borderRadius: "4px",
-									margin: "5px",
-								}}
-							>
-								{answer.answers_title}
-							</div>
-						</div>
-					))}
-			</div> */}
-			{/* <div className="row justify-content-center">
-				{questionInfo.question_answers.slice(2).map((answer, index) => (
-					<div key={answer.answers_id} className="col-6 mb-2">
-						<div
-							className="text-center p-3"
-							style={{
-								backgroundColor: colors[index + 2],
-								color: "black",
-								height: "100px", // Mismo alto para todas las tarjetas
-								display: "flex",
-								alignItems: "center",
-								justifyContent: "center",
-								borderRadius: "4px",
-								margin: "5px",
-							}}
-						>
-							{answer.answers_title}
-						</div>
-					</div>
-				))}
-			</div> */}
 
 			<div className="row">
 				<div className="col text-center">
