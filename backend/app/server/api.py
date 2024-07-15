d828cd72f9ed2fde7cadf2dc7bcf35d650cc8c1a
Carlos Cuenca <carlos.cuenca@upm.es>
2024-07-15 15:34:56 +0200

    feat: allow all origins from port 3000

--- backend/app/server/api.py
+++ backend/app/server/api.py
@@ -28,6 +28,7 @@ app = FastAPI()
 app.add_middleware(
 	CORSMiddleware,
 	allow_origins=["http://localhost:3000"],  # Permite las solicitudes desde el puerto 3000
+	allow_origin_regex='http://.*:3000',
 	allow_credentials=True,
 	allow_methods=["*"],  # Permite todos los métodos
 	allow_headers=["*"],  # Permite todos los encabezados
